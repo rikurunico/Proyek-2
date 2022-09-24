240be218bb4e168fc1b3eecb988acdaf682ffbbf
nicoco1173 <rikurunico@gmail.com>
2022-09-24 15:28:03 +0700

    Add More CRUD

--- cypress/e2e/KosSemanggiTest/dormitories.cy.js
+++ cypress/e2e/KosSemanggiTest/dormitories.cy.js
@@ -18,23 +18,31 @@ describe("User Can Open Dormitories Page", () => {
     });
 
     it("User Can Edit Data In Dormitories Page", () => {
-        cy.visit("http://127.0.0.1:8000/dashboard/dormitory/1/edit");
-        cy.get("#name").clear().type("Wazir");
-        cy.get("#address").clear().type("Jl. Semanggi Barat 18");
-        cy.get("#phone_number").clear().type("081234567111");
+        cy.visit("http://127.0.0.1:8000/dashboard");
+        cy.get(":nth-child(6) > .nav-link").click();
+        cy.get('[href="http://127.0.0.1:8000/dashboard/dormitory"]').click();
+        cy.get(".btn-warning").click();
+        cy.get("#name").clear().type("Nicoco");
+        cy.get("#address").clear().type("Jl. Semanggi Timur 17");
+        cy.get("#phone_number").clear().type("081234511111");
         cy.get("form > .btn").click();
     });
 
     it("User Can Read Data In Dormitories Page", () => {
-        cy.visit("http://127.0.0.1:8000/dashboard/dormitory/1");
+        cy.visit("http://127.0.0.1:8000/dashboard");
+        cy.get(":nth-child(6) > .nav-link").click();
+        cy.get('[href="http://127.0.0.1:8000/dashboard/dormitory"]').click();
+        cy.get("tr > .d-flex > .btn-primary").click();
         cy.get(":nth-child(1) > .col-md-7 > .form-control").should(
             "have.text",
-            "Wazir"
+            "Nicoco"
         );
     });
 
     it("User Can Delete Data In Dormitories Page", () => {
-        cy.visit("http://127.0.0.1:8000/dashboard/dormitory/1");
+        cy.visit("http://127.0.0.1:8000/dashboard");
+        cy.get(":nth-child(6) > .nav-link").click();
+        cy.get('[href="http://127.0.0.1:8000/dashboard/dormitory"]').click();
         cy.get(".btn-danger").click();
         cy.get("h3").should("have.text", "Tidak ada Data Penghuni");
     });
